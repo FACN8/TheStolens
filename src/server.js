19cdeb8cdc0b9c49cfda2daddd420fa01c88766e
hadeel11 <faour.hadeel@gmail.com>
2020-01-29 15:29:57 +0200

    Creates server

---
+++ src/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const http = require("http");
+const port = process.env.PORT || 9000;
+const router = require("../src/router.js");
+const server = http.createServer(router);
+
+server.listen(port, () => {
+  console.log("Now listening on port " + port);
+});
